4f5b0cec9518f79bbff9e547a947955043ea1667
Manish Kumar <manishrajkumar856@gmail.com>
2026-02-12 18:32:56 +0530

    Day 15 Completed!

---
+++ day-15-Project-1-bcrypt-Cr_Post-Image_Kit-Upload_Img_Data/backend/src/controller/post.controller.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const ImageKit = require("@imagekit/nodejs");
+const {toFile} = require("@imagekit/nodejs");
+
+
+/**
+ * Image Kit Usage - (Initialization)
+ */
+const imagekit = new ImageKit({
+  
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  
+});
+
+
+async function createPost(req, res) {
+  console.log(req.file);
+
+  /**
+   * ImageKit Usage
+   */
+  const file = await imagekit.files.upload({
+    file: await toFile(Buffer.from(req.file.buffer), 'file'),
+    fileName: "Test",
+  });
+
+  res.status(200).json({
+    file,
+    message: "File Send Successfully!",
+  });
+}
+
+module.exports = { createPost };
